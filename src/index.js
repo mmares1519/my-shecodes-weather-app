01f5e59acb6bf030b7373c40611e1871336b3d67
mmares1519 <marissamares6@yahoo.com>
2024-01-02 14:21:25 -0800

    fixed CSS

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ function handleSearchSubmit(event) {
 }
 
 function displayForecast() {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   let forecastHtml = "";
 
   days.forEach(function (day) {
